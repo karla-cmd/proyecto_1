d2beba9428254bdc760d771dfdbff04cf2ea5800
Karla <katy505779@gmail.com>
2026-06-27 00:29:38 +0200

    Inicializar proyecto_1 con estructura base

---
+++ main.py
@@ -0,0 +1,6 @@
+def saludar(nombre):
+    """Función que saluda a una persona."""
+    return f"Hola, {nombre}! Bienvenido a ML."
+if __name__ == '__main__':
+    mensaje = saludar("Estudiante")
+print(mensaje) 
